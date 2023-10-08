71c8c5cfcbdc0d71665c7bbe5cd68d7fe6089e1d
mamtawardhani <53176352+mamtawardhani@users.noreply.github.com>
2023-10-08 15:19:04 +0530

    Update Login.js
    furst

--- screens/Login.js
+++ screens/Login.js
@@ -12,7 +12,6 @@ import {
 } from "react-native";
 import firebase from "firebase";
 
-const bgImage = require("../assets/background1.png");
 const appIcon = require("../assets/appIcon.png");
 const appName = require("../assets/appName.png");
 
@@ -34,29 +33,22 @@ export default class LoginScreen extends Component {
     const { email, password } = this.state;
     return (
       <KeyboardAvoidingView behavior="padding" style={styles.container}>
-        <ImageBackground source={bgImage} style={styles.bgImage}>
+        <ImageBackground style={styles.bgImage}>
           <View style={styles.upperContainer}>
-            <Image source={appIcon} style={styles.appIcon} />
-            <Image source={appName} style={styles.appName} />
+           
           </View>
           <View style={styles.lowerContainer}>
             <TextInput
               style={styles.textinput}
-              onChangeText={text => this.setState({ email: text })}
-              placeholder={"Enter Email"}
-              placeholderTextColor={"#FFFFFF"}
-              autoFocus
+              
             />
             <TextInput
               style={[styles.textinput, { marginTop: 20 }]}
-              onChangeText={text => this.setState({ password: text })}
-              placeholder={"Enter Password"}
-              placeholderTextColor={"#FFFFFF"}
-              secureTextEntry
+              
             />
             <TouchableOpacity
               style={[styles.button, { marginTop: 20 }]}
-              onPress={() => this.handleLogin(email, password)}
+            
             >
               <Text style={styles.buttonText}>Login</Text>
             </TouchableOpacity>
